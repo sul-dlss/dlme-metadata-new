ee4a4949f253d3ac52ee01c7146ff11ea0678a1c
Jacob Hill <jacobhill.mail@gmail.com>
2019-02-07 14:15:12 -0800

    fix harvesting script add ssm data

--- sakip-sabanci/sakip-sabanci-harvest.py
+++ sakip-sabanci/sakip-sabanci-harvest.py
@@ -1,6 +1,5 @@
 import errno
 import os
-from lxml import etree
 from sickle import Sickle
 from sickle.iterator import OAIResponseIterator
 
@@ -12,41 +11,30 @@ def to_str(bytes_or_str):
         value = bytes_or_str
     return value  # Instance of str
 
-sickle = Sickle('http://cdm21044.contentdm.oclc.org/oai/oai.php', iterator=OAIResponseIterator)
+sickle = Sickle('http://cdm21044.contentdm.oclc.org/oai/oai.php')
+print("Sickle instance created.") # status update
 
-sets = ['Kitapvehat', 'ResimKlksyn', 'emirgan', 'abidindino']
+sets = sickle.ListSets()
+print("Sets created.") # status update
 
-for item in sets:
-    records = sickle.ListRecords(metadataPrefix='oai_dc', set=item)
-    file_name = '{}/data/{}.xml'.format(item, item)
-    if not os.path.exists(os.path.dirname(file_name)):
-        try:
-            os.makedirs(os.path.dirname(file_name))
-        except OSError as exc: 
-            if exc.errno != errno.EEXIST:
-                raise
-    
-    with open(file_name, 'w') as f:
-        for page in records:
-            f.write(to_str(records.next().raw.encode('utf8')))
-    
-    f.close()
-
-# # Parsing into one record per file
-
-# files = []
-# for item in set:
-#     files.append('{}/data/{}.xml'.format(item, item))
-
-# for file in files:
-#     tree = etree.parse(file)
-#     records = etree.Xpath("records/record") 
-#     records = tree.findall("record") 
-#     file_count = 1
-#     for record in records:
-#         if not record.get("status") = "deleted":
-#             next_file = open('{}/data/{}-{}.xml'.format(item, item, file_count), 'w')
-#             next_file.write(etree.tostring(record, encoding='unicode', pretty_print=True))
-#             file_count += 1
-#             next_file.close()
-#     #os.remove(file)
+set_number = 0
+for s in sets:
+    records = sickle.ListRecords(metadataPrefix="oai_dc", set=s.setSpec, ignore_deleted=True)
+    print("Records created.") # status update
+    file_count = 1
+    set_number += 1
+    record_number = 1
+    for record in records:
+        print("Set number: " + str(set_number) + " | " + "Record number " + str(record_number))
+        record_number += 1
+        if not os.path.exists(os.path.dirname('{}/data/{}-{}.xml'.format(s.setSpec, s.setSpec, file_count))):
+            try:
+                os.makedirs(os.path.dirname('{}/data/{}-{}.xml'.format(s.setSpec, s.setSpec, file_count)))
+            except OSError as exc: 
+                if exc.errno != errno.EEXIST:
+                    raise
+        with open('{}/data/{}-{}.xml'.format(s.setSpec, s.setSpec, file_count), 'w') as f:
+        	file_count += 1
+        	f.write(to_str(record.raw.encode('utf8')))
+        	f.close()
+    		
